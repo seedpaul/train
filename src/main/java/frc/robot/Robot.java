4df9541ce9dec8366e0ec94c7c908c99d20b7fa6
seedpaul <paul.seed@gmail.com>
2024-12-07 14:32:00 -0500

    changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -55,7 +55,7 @@ public class Robot extends TimedRobot {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    driveTrain.arcadeDrive(sidewinder.getRawAxis(1),sidewinder.getRawAxis(0));
+    driveTrain.arcadeDrive(sidewinder.getRawAxis(0),-sidewinder.getRawAxis(1));
     
     double speed =  sidewinder.getRawAxis(2);
 
